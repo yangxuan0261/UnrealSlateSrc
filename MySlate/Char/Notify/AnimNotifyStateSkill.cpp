48121a5110d94af41a3d02b2d2bdbaaaa7749573
杨旋 <yx0588@175game.com>
2016-03-28 16:21:10 +0800

    - add AnimNotifyStateSkill.h

---
+++ MySlate/Char/Notify/AnimNotifyStateSkill.cpp
@@ -0,0 +1,59 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MySlate.h"
+#include "AnimNotifyStateSkill.h"
+
+#include "Char/MyChar.h"
+
+DECLARE_LOG_CATEGORY_EXTERN(UAnimNotifyLogger, Log, All);
+DEFINE_LOG_CATEGORY(UAnimNotifyLogger)
+
+// Sets default values
+UAnimNotifyStateSkill::UAnimNotifyStateSkill(): Super()
+{
+	mMyChar = nullptr;
+}
+
+UAnimNotifyStateSkill::~UAnimNotifyStateSkill()
+{
+
+}
+
+void UAnimNotifyStateSkill::NotifyBegin(USkeletalMeshComponent * MeshComp, UAnimSequenceBase * Animation, float TotalDuration)
+{
+	Super::NotifyBegin(MeshComp, Animation, TotalDuration);
+
+	if (mMyChar == nullptr)
+		mMyChar = Cast<AMyChar>(MeshComp->GetOwner());
+
+	if (mMyChar != nullptr)
+	{
+		//create bullet
+
+	}
+	else
+	{
+		UE_LOG(UAnimNotifyLogger, Warning, TEXT("--- UAnimNotifyStateSkill::NotifyBegin, mMyChar == nullptr"));
+
+	}
+
+}
+
+void UAnimNotifyStateSkill::NotifyTick(USkeletalMeshComponent * MeshComp, UAnimSequenceBase * Animation, float FrameDeltaTime)
+{
+	Super::NotifyTick(MeshComp, Animation, FrameDeltaTime);
+	if (mMyChar != nullptr)
+	{
+
+	}
+}
+
+void UAnimNotifyStateSkill::NotifyEnd(USkeletalMeshComponent * MeshComp, UAnimSequenceBase * Animation)
+{
+	Super::NotifyEnd(MeshComp, Animation);
+	if (mMyChar != nullptr)
+	{
+		//begin shoot
+
+	}
+}
---
+++ MySlate/Char/Notify/AnimNotifyStateSkill.h
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Animation/AnimNotifies/AnimNotifyState.h"
+#include "AnimNotifyStateSkill.generated.h"
+
+class AMyChar;
+
+UCLASS()
+class UAnimNotifyStateSkill : public UAnimNotifyState
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's 
+	UAnimNotifyStateSkill();
+	virtual ~UAnimNotifyStateSkill();
+
+public:
+	virtual void NotifyBegin(USkeletalMeshComponent * MeshComp, UAnimSequenceBase * Animation, float TotalDuration) override;
+	virtual void NotifyTick(USkeletalMeshComponent * MeshComp, UAnimSequenceBase * Animation, float FrameDeltaTime) override;
+	virtual void NotifyEnd(USkeletalMeshComponent * MeshComp, UAnimSequenceBase * Animation) override;
+
+public:
+	AMyChar*		mMyChar;
+};
